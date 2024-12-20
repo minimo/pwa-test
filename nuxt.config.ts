19f118858e76ce30f3d7e5ea210b76d1cfc511c9
minimo <minimo.mobile@gmail.com>
2024-12-20 12:19:13 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true
+  },
+  app: {
+    head: {
+      title: 'PWA-TEST',
+      link: [
+        { rel: 'manifest', href: '/manifest.webmanifest' }
+      ]
+    }
+  },
 })
