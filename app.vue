6ba6b2136526745b0806435e4e6b9fef84cc29f2
minimo <minimo.mobile@gmail.com>
2024-12-21 10:43:38 +0900

    update

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup>
+
+const onClick = () => {
+  document.documentElement.requestFullscreen()
+}
+
+</script>
+
 <template>
   <div>
     2024-12-20-3
+    <button @click="onClick">
+      フルスクリーン
+    </button>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
